58cda0ae1b8ca755dc10b200d262648edf1c7bba
willnet <netwillnet@gmail.com>
2022-08-24 18:27:08 +0900

    Add main features roughly

--- lib/capybara/wait_before_click.rb
+++ lib/capybara/wait_before_click.rb
@@ -1,10 +1,32 @@
 # frozen_string_literal: true
 
 require_relative "wait_before_click/version"
+require "capybara"
 
 module Capybara
   module WaitBeforeClick
-    class Error < StandardError; end
-    # Your code goes here...
+    def _wait_for_image_loading
+      Timeout.timeout(Capybara.default_max_wait_time) do
+        sleep 0.5 until evaluate_script(<<~JS)
+        Array.prototype.every.call(
+          document.querySelectorAll('img'),
+          (e) => e.complete
+        )
+        JS
+      end
+    rescue Timeout::Error
+      Rails.logger.debug 'timeout _wait_for_image_loading'
+    end
+
+    def click(*keys, **options)
+      _wait_for_image_loading
+      super
+    end
+  end
+
+  module Node
+    class Element < Base
+      prepend WaitBeforeClick
+    end
   end
 end
