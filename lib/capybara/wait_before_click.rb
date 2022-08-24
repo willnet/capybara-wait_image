af1de308edec62099efd0ec608a193373f92c412
willnet <netwillnet@gmail.com>
2022-08-24 18:13:51 +0900

    bundle gem capybara-wait_before_click

---
+++ lib/capybara/wait_before_click.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "wait_before_click/version"
+
+module Capybara
+  module WaitBeforeClick
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
