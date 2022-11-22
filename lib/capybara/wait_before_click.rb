17bc7eb8e9b9dffd050099095b8bb6486d87fbc1
willnet <netwillnet@gmail.com>
2022-11-22 14:05:43 +0900

    Each problem that emerged in the actual use case was addressed.
    - Addressed Capybara::NotSupportedByDriverError when using rack-test. rack-test does not load images, so it can be ignored.
    
    - Sometimes Selenium::WebDriver::Error::StaleElementReferenceError occurs, so I tried to load the document again and retry to fix it.

--- lib/capybara/wait_before_click.rb
+++ lib/capybara/wait_before_click.rb
@@ -15,12 +15,27 @@ module Capybara
         JS
       end
     rescue Timeout::Error
-      Rails.logger.debug 'timeout _wait_for_image_loading'
+      _logger.debug '[capybara-wait_before_click]Timeout::Error'
+    rescue Capybara::NotSupportedByDriverError
+      # It comes here when you run it in rack-test, but you can ignore it
+    rescue Selenium::WebDriver::Error::StaleElementReferenceError
+      _logger.debug '[capybara-wait_before_click]Selenium::WebDriver::Error::StaleElementReferenceError'
+      reload
+      _wait_for_image_loading
     end
 
     def click(*keys, **options)
       _wait_for_image_loading
       super
+    end
+
+    def _logger
+      @_logger ||= if defined?(Rails)
+                     Rails.logger
+                   else
+                     Logger.new('/dev/null')
+                   end
+      end
     end
   end
 
--- spec/capybara/wait_before_click_spec.rb
+++ spec/capybara/wait_before_click_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Capybara::WaitBeforeClick do
   it "has a version number" do
     expect(Capybara::WaitBeforeClick::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
